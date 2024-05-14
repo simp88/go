60994e40647def6c6d2bea55271df6238de4b758
simp88 <simpleness66@gmail.com>
2024-05-14 22:57:23 +0800

    [添加]条件控制文件

---
+++ src/project1/条件控制.go
@@ -0,0 +1,65 @@
+package main
+
+import "fmt"
+
+/*func main() {
+	a, b := 1, 10
+	if a > b {
+		b++
+	} else {
+		b--
+	}
+	fmt.Println(a)
+	fmt.Println(b)
+}*/
+
+/*func main() {
+	score := 900
+	var ans string
+	if score >= 100 {
+		ans = "S"
+	} else if score >= 90 && score < 100 {
+		ans = "A"
+	} else if score >= 80 && score < 90 {
+		ans = "B"
+	} else if score >= 70 && score < 80 {
+		ans = "C"
+	} else if score > 60 && score < 70 {
+		ans = "E"
+	}
+	fmt.Println(ans)
+}
+*/
+
+/*func main() {
+	str := "as"
+	switch str {
+	case "a":
+		str += "a"
+		str += "c"
+	case "b":
+		str += "bb"
+		str += "aaaa"
+	default: // 当所有case都不匹配后，就会执行default分支
+		str += "CCCC"
+	}
+	fmt.Println(str)
+}
+*/
+
+func main() {
+	switch num := f(); {
+	case num >= 0 && num <= 1:
+		num++
+	case num > 1:
+		num--
+		fallthrough
+	case num < 0:
+		num += num
+	} fmt.Println("num")
+}
+func f() int {
+	return 1
+}
+
+
--- src/project1/输入输出.go
+++ src/project1/输入输出.go
@@ -1,15 +1,55 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"os"
 )
 
 func main() {
-	os.Stdout.WriteString("Hello 世界!")
+	/*os.Stdout.WriteString("Hello 世界!")*/
 	println("")
 	println("Hello 世界!")
 	fmt.Println("Hello 世界！")
+
 	fmt.Printf("%%%s\n", "hello world")
+
+	fmt.Printf("%b\n", 2<<7-1)
+
+	fmt.Printf("%f\n", 1e2)
+	//使用其它进制时，在%与格式化动词之间加上一个空格便可以达到分隔符的效果
+
+	fmt.Println()
+	str := "abcdefg"
+	fmt.Printf("%x\n", str)
+	fmt.Printf("% x\n", str)
+
+	fmt.Printf("%s\n", "hello world")
+
+	fmt.Println("--------------")
+
+	/*	var s, s2 string
+		fmt.Scanln(&s, &s2)
+		fmt.Println(s, s2)
+	*/
+	/*	var s, s2, s3 string
+		scanf, err := fmt.Scanf("%s %s %s", &s, &s2, &s3)
+		if err != nil {
+			fmt.Println(scanf, err)
+		}
+		fmt.Println(s)
+		fmt.Println(s2)
+		fmt.Println(s3)*/
+
+	/*	//读
+		scanner := bufio.NewScanner(os.Stdin)
+		scanner.Scan()
+		fmt.Println(scanner.Text())*/
+
+	//写
+	writer := bufio.NewWriter(os.Stdout)
+	writer.WriteString("hello world! \n")
+	writer.Flush()
+	fmt.Println(writer.Buffered())
 
 }
